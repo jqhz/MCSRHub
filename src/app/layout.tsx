22f58871f864abdd4a62f0f50fee593ef75b0b57
jahzz <jasonhuang3030@gmail.com>
2026-01-30 23:56:42 -0600

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from 'next';
 import '@src/styles/globals.css';
 import AppShell from '@src/components/AppShell';
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: 'MCSR Hub',
   description: 'MCSR community hub',
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AppShell>{children}</AppShell>
+        <Analytics />
       </body>
     </html>
   );
